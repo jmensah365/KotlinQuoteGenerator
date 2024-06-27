c50621d3acdba847e239a733ecfd878e245c20f3
jmensah365 <66219183+jmensah365@users.noreply.github.com>
2024-06-27 12:32:28 -0400

    Update QuoteAPI.kt

--- app/src/main/java/com/example/quotegenerator/QuoteAPI.kt
+++ app/src/main/java/com/example/quotegenerator/QuoteAPI.kt
@@ -2,8 +2,11 @@ package com.example.quotegenerator
 import retrofit2.Response
 import retrofit2.http.GET
 
+//creating getter api
 interface QuoteAPI {
+    //pass in the path of the api
     @GET("random")
+    //creating an async function to get response from API
     suspend fun getRandomQuote() : Response<List<QuoteModel>>
 
 }
