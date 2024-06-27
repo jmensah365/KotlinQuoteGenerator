4cc960270c4d9fefecc703ec48c1fcd5b3c4ad80
jmensah365 <66219183+jmensah365@users.noreply.github.com>
2024-06-27 12:37:38 -0400

    Update RetroFitInstance.kt

--- app/src/main/java/com/example/quotegenerator/RetroFitInstance.kt
+++ app/src/main/java/com/example/quotegenerator/RetroFitInstance.kt
@@ -4,15 +4,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetroFitInstance {
+    //base url for the API
     private const val BASE_URL = "https://zenquotes.io/api/"
 
+    //function to create and configure a retrofit instance 
     private fun getInstance(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create()) //handles JSON deserialization
             .build()
 
     }
-
+    //Quote API instance... The QuoteAPI interface will be implemented by Retrofit, providing the API's methods
     val quoteApi : QuoteAPI = getInstance().create(QuoteAPI::class.java)
 }
