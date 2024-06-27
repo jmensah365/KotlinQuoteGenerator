5f3ad8802f450141445db015ff6e9385ade84a2f
jmensah365 <66219183+jmensah365@users.noreply.github.com>
2024-06-27 12:30:54 -0400

    Adding comments

--- app/src/main/java/com/example/quotegenerator/MainActivity.kt
+++ app/src/main/java/com/example/quotegenerator/MainActivity.kt
@@ -9,23 +9,27 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    //creating binding for variables and views
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //assigning views with binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         getQuote()
-
+        //adding next btn functionality
         binding.nextBtn.setOnClickListener {
             getQuote()
         }
     }
+    //calling the Quote Api
     private fun getQuote() {
         setInProgress(true)
 
         GlobalScope.launch {
             try {
+                //grabbing quote and author from response and setting the UI with those values
                 val response = RetroFitInstance.quoteApi.getRandomQuote()
                 runOnUiThread{
                     setInProgress(false)
@@ -33,6 +37,7 @@ class MainActivity : AppCompatActivity() {
                         setUI(it)
                     }
                 }
+                //if not internet connection throw error
             } catch (e : Exception) {
                 runOnUiThread {
                     setInProgress(false)
@@ -41,10 +46,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+    //sets the quote and author values
     private fun setUI(quoteModel: QuoteModel){
         binding.quoteTv.text = quoteModel.q
         binding.authorTv.text = quoteModel.a
     }
+    //checking to see if quotes are visible
     private fun setInProgress(inProgress: Boolean) {
         if(inProgress) {
             binding.progressBar.visibility = View.VISIBLE
